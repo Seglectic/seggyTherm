47117402b6a7cb62c69090ea53e0dec612ed0fcc
Segger <saintdane@gmail.com>
2020-07-23 10:01:29 -0400

    Added datalogging, still buggy tho
    Removed button input processing and ISR
    
    Added Charts.js chart to display data but
    malformed data is making its way in to fuck things up

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,33 @@
 
 
 
-
-/********************************************************************************
+/****************************************************************************************************************************************************************
  *  
- *                                 Libraries 📚
+ *                                                          Libraries 📚
  *  
- ********************************************************************************/
-#include <Arduino.h>             // Arduino library
-#include <Wire.h>                // Required for i²C comms
-#include <Adafruit_SSD1306.h>    // SSD1306 display driver
-#include <Adafruit_GFX.h>        // GFX Library for drawing to display
-#include "DHTesp.h"              // Library for DHT22 temp / humidity sensor
+ ****************************************************************************************************************************************************************/ 
+#include <Arduino.h>
+#include <DHTesp.h>                // Library for DHT22 temp / humidity sensor
+#include <Adafruit_SSD1306.h>      // SSD1306 display driver
+#include <Wire.h>                  // Required for i²C comms
+#include <Adafruit_GFX.h>          // GFX Library for drawing to display
+#include <FS.h>                    // SPIFFS File system https://arduino-esp8266.readthedocs.io/en/latest/filesystem.html
+#include <ESP8266WiFi.h>           // Main WiFi library https://github.com/esp8266/Arduino
+#include <WiFiManager.h>           // WiFi Manager for captive library and autoconnect https://github.com/tzapu/WiFiManager
+#include <DNSServer.h>             // ESP8266 DNS required for captive portal
+#include <ESP8266WebServer.h>      // Main web server library
+#include <WiFiUDP.h>               // UDP required for NTP & OTA updates
+#include <NTPClient.h>             // Network Time Protocol client
+#include <ArduinoOTA.h>            // Allows updating firmware wirelessly https://docs.platformio.org/en/latest/platforms/espressif8266.html#over-the-air-ota-update
 
 
 
-/********************************************************************************
- * 
- *                                 I/O Setup 📺
- *     
- ********************************************************************************/
+
+/****************************************************************************************************************************************************************
+ *  
+ *                                                          I/O Setup 📺
+ *  
+ ****************************************************************************************************************************************************************/ 
 
 #define SCREEN_WIDTH   128                                                    // OLED display width, in pixels
 #define SCREEN_HEIGHT  64                                                     // OLED display height, in pixels
@@ -43,43 +51,139 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);     // Initialize display object as 'display'
 DHTesp dht;                                                                   // Initialize temp sensor as 'dht'
 
+String Version = "0.00.000.000.00.000.1a";
 
-// Setup button code
-const int buttonPin = D5;
-bool buttonFlag = 0;                                                          //Has button been pushed? Queue action if so
-int buttonCounter = 0;
+int saveTimer = 0;
+int saveInterval = 60000;
+int lastMillis = 0;
 
-// Button Interrupt Service Routine
-void ICACHE_RAM_ATTR buttonISR() {                                            // ICACHE_RAM_ATTR caches this function into 
-  buttonFlag = true;                                                          // ESP RAM at runtime. Keep compact.
+/****************************************************************************************************************************************************************
+ *  
+ *                                                          NTP Setup ⌚
+ *  
+ ****************************************************************************************************************************************************************/ 
+
+WiFiUDP ntpUDP;                                                     // Configure NTP Client with -14400 seconds                                     
+NTPClient timeClient(ntpUDP,"pool.ntp.org",0,86400000);             // offset for GMT-5 and 24h update interval (8.64e+7ms)
+// NTPClient timeClient(ntpUDP,"pool.ntp.org",-14400,60);           // offset for GMT-5 and 60 second update interval
+
+
+
+/****************************************************************************************************************************************************************
+ *  
+ *                                                          Web Server File Handling 📂
+ *  
+ ****************************************************************************************************************************************************************/ 
+
+ESP8266WebServer server(80);                             //Create web server
+
+
+ /* * * * * * * * * * * * * * * * * * * * * *
+ *             getContentType()         
+ *            
+ *    Function to return 'Content-Type'
+ *    based on file extension for
+ *    proper client formatting
+ * 
+ *  todo: replace this with switchcase
+ *  https://www.arduino.cc/reference/en/language/structure/control-structure/switchcase/
+ * 
+  * * * * * * * * * * * * * * * * * * * * */
+String getContentType(String filename) { 
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  return "text/plain";
 }
 
-//Check for button input
-bool handleButton(){
-  if(digitalRead(buttonPin)==LOW){
-    return false;
-  }else{
+
+ /* * * * * * * * * * * * * * * * * * * * * *
+ *            handleFileRead()
+ *             
+ *    Sends the right file to the client
+ *    and sends the file else returns false
+ * 
+  * * * * * * * * * * * * * * * * * * * * */
+
+bool handleFileRead(String path) { 
+  Serial.println("Serving file: " + path);
+  if (path.endsWith("/")) path += "index.html";         // If a folder is requested, send the index file (LOCAL LINKS TO FOLDERS MUST END WITH / )
+  String contentType = getContentType(path);            // Get the MIME type
+  if (SPIFFS.exists(path)) {                            // If the file exists
+    File file = SPIFFS.open(path, "r");                 // Open it
+    server.streamFile(file, contentType);               // And send it to the client
+    file.close();                                       // Then close the file again
     return true;
   }
+  return false;                                         // If the file doesn't exist, return false
 }
 
-// Determines what will appear on screen: 0 Local temp sensor | 1 Remote temp sensor | 2 Server / wifi stats? |
-int displayMode = 0;
+
+/****************************************************************************************************************************************************************
+ * 
+ *                                                        Web Response Handlers 👋
+ *     
+ ****************************************************************************************************************************************************************/ 
 
 
-/********************************************************************************
+ /* * * * * * * * * * * * * * * * * * * * *
+ *             handleGeneric()
+ *             
+ *     Example response handler with a
+ *  sample parameter check and response
+
+  * * * * * * * * * * * * * * * * * * * * */
+void handleGeneric() { 
+  String response = "Number of Parameters: ";                     // Create response string to send
+  response += server.args();                                      // Get number of parameters
+  response += "\n";                                               // New line (<br> for html response)
+  for (int i = 0; i < server.args(); i++) {  
+    response += "Arg #" + (String)i + " -> ";                     // Include the current iteration value
+    response += server.argName(i) + ": ";                         // Get the name of the parameter
+    response += server.arg(i) + "\n";                             // Get the value of the parameter
+  } 
+  if(server.arg("blep")){                                         // Check if parameter exists
+    response+="Client sent a blep parameter!!!\n";
+  }
+  server.send(200, "text/plain", response);                       // Respond to the HTTP request with plaintext
+}
+
+
+void getVersion(){
+  server.send(200,"text/plain",Version);
+}
+
+// Send temperature and humidity data
+void getTemp(){
+
+  float tempC = dht.getTemperature();
+  float tempF = tempC*1.8+32;
+  float humidity = dht.getHumidity();
+  String response = "";
+  response += String(tempF) + " ";
+  response += String(humidity);
+
+  server.send(200,"text/plain",response);
+}
+
+
+
+
+
+
+/****************************************************************************************************************************************************************
  *  
- *                                 Main Setup 🚧
+ *                                                          Main Setup 🚧
  *  
- ********************************************************************************/
+ ****************************************************************************************************************************************************************/ 
 
 void setup() {
   Serial.begin(115200);
 
   dht.setup(D4, DHTesp::DHT22);         //Setup dht as a DHT22
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(D4,INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(buttonPin),buttonISR,RISING); // Setup button interrupt 
 
   // float tempC = dht.getTemperature();
   // float tempF = tempC*1.8+32;
@@ -100,33 +204,55 @@ void setup() {
   display.println(F("Seglectic Softworks")); 
   display.println(F("                -2020"));
   display.display();
-  delay(500);
-  
-// iono
-  // display.clearDisplay();
-  // display.setCursor(0,0);
-  // display.setTextSize(1); 
-  // display.println("WiFi Connected.");
-  // display.println("\n");
-  // display.setTextSize(1); 
   
   display.display();
   // delay(800);
   display.clearDisplay();
   display.display();
        
+
+ /* * * * * * * * * * * *
+  *   WifiManager Setup
+  * * * * * * * * * * * */   
+
+  WiFiManager wifiManager;                               //Setup WiFi Manager
+  wifiManager.autoConnect("segSetup","seglectic");       //Autoconnect; else start portal with this SSID, P/W
+
+  server.on("/generic",handleGeneric);
+  server.on("/version",getVersion   );
+  server.on("/temp",getTemp         );
+
+  //Serves file to client
+  server.onNotFound([]() {                                        // If the client requests any URI
+    if (!handleFileRead(server.uri()))                            // send it if it exists
+      server.send(404, "text/plain", "404'd");                    // otherwise, respond with a 404 (Not Found) error
+  });
+
+
+ /* * * * * * * * * * * *
+  *   Start the things
+  * * * * * * * * * * * */   
+  server.begin();                                                 // Starts file server
+  SPIFFS.begin();                                                 // Starts local file system
+  timeClient.begin();                                             // Starts NTP client
+
+Serial.println(timeClient.getFormattedTime());
 }
 
-void localTemps(){
 
+ /* * * * * * * * * * * * * * * * * * * * *
+ *             localTemp()
+ *             
+ *     Displays Local temps to screen
+  * * * * * * * * * * * * * * * * * * * * */
+void localTemps(){
+  display.clearDisplay();
   // Get data from sensors
   float tempC = dht.getTemperature();
   float tempF = tempC*1.8+32;
   float humidity = dht.getHumidity();
   
-
   // display.fillRect(0,0,128,53,BLACK);           // Clear top area (x,y,w,h,clr)
-
   display.setTextSize(3);         
   display.setTextColor(WHITE);                  
   display.setCursor(0,17);
@@ -153,53 +279,54 @@ void localTemps(){
 
     int TY = map(tempF,50,100,0,128);
     display.fillRect(0,0,TY,14,WHITE);
-    delay(500); 
   }
-
-
+  display.display();
 }
 
 
 
+ /* * * * * * * * * * * * * * * * * * * * *
+ *             logData()
+ *             
+ *     Logs temp (metric) & humidity data
+ *  to weather.txt file for reading from browser
+  * * * * * * * * * * * * * * * * * * * * */
+
+ bool logData(){
+   int m = millis();
+   saveTimer+= m-lastMillis;
+   lastMillis = m;
+
+   if(saveTimer>saveInterval){
+    saveTimer=0;
+    File saveFile = SPIFFS.open("/weather.txt","a");                  // Open save file in append mode
+    if(!saveFile){                                                    
+      Serial.println("failed to open save file");                     // Check if file opened
+      return false;
+    }else{
+      String saveData = String(timeClient.getEpochTime()) +"|"+ String(dht.getTemperature()) + "|" + String(dht.getHumidity());
+      saveFile.println( saveData );                                   // Save color in data param
+      saveFile.close(); 
+      Serial.println("Saved me some data.");
+    }
+   }else{
+     return false;
+   }
+ }
 
 
-/********************************************************************************
- * 
- *                                 Main Loop ➰
+
+
+/****************************************************************************************************************************************************************
  *  
- ********************************************************************************/
+ *                                                          Main Loop ➰
+ *  
+ ****************************************************************************************************************************************************************/ 
 
 void loop() {
-
-  display.clearDisplay();
-
-  if( displayMode==0 ){ localTemps(); }
-  if( displayMode==1 ){ display.clearDisplay(); display.display(); }
+  timeClient.update();
+  localTemps();  
+  logData();
+  server.handleClient();
   
-  //Bottom Line
-  // display.drawLine(0,5,128,53,WHITE);          //Draw fancy line above bottom
-  // display.fillRect(0,55,128,9,WHITE);           //Fill bottom line with white 
-  // display.setTextSize(1);         
-  // display.setTextColor(BLACK);                  //Invert font for bottom line
-  // display.setCursor(1,56);                       //Display raw value from Sensor
-  // display.setCursor(64,56);
-  // Serial.println(handleButton());
-
-
-
-
-  
-   
-
-  // Was the button pushed? Cycle mode if so.
-  if(buttonFlag){
-    if(displayMode<1){
-      displayMode++;
-    }else{
-      displayMode=0;
-    }
-    buttonFlag=false;
-  }
-
-  display.display();
 }
